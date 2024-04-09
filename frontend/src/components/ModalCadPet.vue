58ef75425d7935c8eb9e296f06125a2e33e4651d
Bia <biancavitoriacampos@hotmail.com>
2024-04-09 11:57:07 -0300

    Modal de cadastrar animal já pronta, falta apenas alguns detalhes, e deixar ela funcional!

---
+++ frontend/src/components/ModalCadPet.vue
@@ -0,0 +1,54 @@
+<!-- <template>
+    <Dialog v-model:visible="visible" modal header="Edit Profile" :style="{ width: '25rem' }">
+      <template #header>
+        <div class="inline-flex align-items-center justify-content-center gap-2">
+          <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
+          <span class="font-bold white-space-nowrap">Amy Elsner</span>
+        </div>
+      </template>
+      <span class="p-text-secondary block mb-5">Update your information.</span>
+      <div class="flex align-items-center gap-3 mb-3">
+        <label for="username" class="font-semibold w-6rem">Username</label>
+        <InputText id="username" class="flex-auto" autocomplete="off" />
+      </div>
+      <div class="flex align-items-center gap-3 mb-2">
+        <label for="email" class="font-semibold w-6rem">Email</label>
+        <InputText id="email" class="flex-auto" autocomplete="off" />
+      </div>
+      <template #footer>
+        <Button label="Cancel" text severity="secondary" @click="visible = false" autofocus />
+        <Button label="Save" outlined severity="secondary" @click="visible = false" autofocus />
+      </template>
+    </Dialog>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  import Dialog from 'primevue/dialog';
+  import InputText from 'primevue/inputtext';
+  import Avatar from 'primevue/avatar';
+  import Button from 'primevue/button';
+  
+  export default {
+    components: {
+      Dialog,
+      InputText,
+      Avatar,
+      Button
+    },
+    props: {
+      visible: {
+        type: Boolean,
+        default: false
+      }
+    },
+    setup(props) {
+      const visible = ref(props.visible);
+  
+      return {
+        visible
+      };
+    }
+  };
+  </script>
+   -->
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -61,6 +61,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/PerfilView.vue')
+    },
+    {
+      path: '/modalCadPet',
+      name: 'modalCadPet',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../components/ModalCadPet.vue')
     }
   ]
 })
