81c62ce58c800dfa28f9e1c02319366718e4a89d
Matheus Henry <goncalvessanches.mh@gmail.com>
2024-04-25 21:33:54 -0300

    components

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -13,6 +13,7 @@ declare module 'vue' {
     CardPerfil: typeof import('./src/components/CardPerfil.vue')['default']
     CardPerfilPetShop: typeof import('./src/components/CardPerfilPetShop.vue')['default']
     CardsAnimais: typeof import('./src/components/CardsAnimais.vue')['default']
+    Carousel: typeof import('primevue/carousel')['default']
     Dialog: typeof import('primevue/dialog')['default']
     Dropdown: typeof import('primevue/dropdown')['default']
     FeedAnimais: typeof import('./src/components/FeedAnimais.vue')['default']
